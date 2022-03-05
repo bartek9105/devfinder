d72ea5e7a11502f1c316b7d6dc59d5b446abc479
bartek9105 <bartek9105@gmail.com>
2022-03-05 16:41:40 +0100

    add svgr library

---
+++ src/components/DevFinderLogo/index.tsx
@@ -0,0 +1,7 @@
+import styles from './DevFinderLogo.module.scss'
+
+const DevFinderLogo = () => {
+	return <h1 className={styles.logo}>devfinder</h1>
+}
+
+export default DevFinderLogo
---
+++ src/components/ThemeSwitch/index.tsx
@@ -0,0 +1,8 @@
+import { ReactComponent as SunIcon } from '../../icons/sun.svg'
+import styles from './ThemeSwitch.module.scss'
+
+const ThemeSwitch = () => {
+	return <SunIcon className={styles.sunIcon} />
+}
+
+export default ThemeSwitch
